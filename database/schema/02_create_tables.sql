02c9dcdd4c294729e6097950619c4d49c6f1b77c
mrayyan2001 <mmrian2001@gmail.com>
2025-03-30 18:40:14 +0300

    add shared attributes

--- database/schema/02_create_tables.sql
+++ database/schema/02_create_tables.sql
@@ -1,11 +1,20 @@
 USE FoodtekDB;
 
-
+-- Shared Attributes for all tables
+-- Id INT PRIMARY KEY IDENTITY(1,1),
+-- CreatedAt DATETIME DEFAULT GETDATE(),
+-- CreatedBy INT NULL,
+-- UpdatedAt DATETIME DEFAULT GETDATE(),
+-- UpdatedBy INT NULL,
+-- IsActive BIT DEFAULT 1,
 
 DROP TABLE IF EXISTS Users;
 CREATE TABLE Users
 (
     Id INT PRIMARY KEY IDENTITY(1,1),
-    CreatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-    -- ...
+    CreatedAt DATETIME DEFAULT GETDATE(),
+    CreatedBy INT NULL,
+    UpdatedAt DATETIME DEFAULT GETDATE(),
+    UpdatedBy INT NULL,
+    IsActive BIT DEFAULT 1,
 )
