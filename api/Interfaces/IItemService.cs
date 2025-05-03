ce2dfeb967e6707faa81f3d5a5b50f8509818437
mrayyan2001 <mmrian2001@gmail.com>
2025-05-04 02:07:24 +0300

    Add ItemController and ItemService
    - Created `ItemController` with endpoints:
        - `GET /api/items/{id}`: Get item details by ID.
        - `GET /api/items/top-rated`: Get top 10 rated items.
        - `GET /api/items/recommended`: Get top 10 recommended items.
    - Implemented `IItemService` and `ItemService` using EF Core for item data retrieval.
    - Added `DetailsItemDTO`, `TopRatedItemDTO`, `TopRecommendedItemDTO` for specific responses.
    - Registered `IItemService` in `Program.cs` for dependency injection.

---
+++ api/Interfaces/IItemService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.DTOs.Item;
+using api.Models;
+
+namespace api.Interfaces
+{
+    public interface IItemService
+    {
+        public Task<DetailsItemDTO?> GetByIdAsync(int id);
+        public Task<List<TopRatedItemDTO>> GetTop10RatedAsync();
+        public Task<List<TopRecommendedItemDTO>> GetTop10RecommendedAsync();
+    }
+}
